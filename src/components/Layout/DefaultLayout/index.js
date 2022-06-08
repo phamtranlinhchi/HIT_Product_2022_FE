ea23ee80afc3585a0d830472189127ea7d717777
izme <91523388+phamtranlinhchi@users.noreply.github.com>
2022-06-09 00:06:34 +0700

    layout frame

---
+++ src/components/Layout/DefaultLayout/index.js
@@ -0,0 +1,19 @@
+import Header from './Header';
+import Footer from './Footer';
+
+import React from 'react';
+
+function DefaultLayout({ children }) {
+    return (
+        <div>
+            <Header />
+            <div className="container">
+                <div className="content">{children}</div>
+            </div>
+
+            <Footer />
+        </div>
+    );
+}
+
+export default DefaultLayout;
